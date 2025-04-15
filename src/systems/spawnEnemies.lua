d0b00ba42fc0a25c448c04782e2bb372604979c4
Andrea D'Amico <andrea.damico@gmail.com>
2025-04-15 21:28:18 +0200

    fix spawnenemies

--- src/systems/spawnEnemies.lua
+++ src/systems/spawnEnemies.lua
@@ -1,25 +1,25 @@
+local chance = require('chance')
+
 local spawnEnemies = ecs.processingSystem(class('SpawnEnemies'))
 local Enemy = require('enemy')
 
 spawnEnemies.filter = ecs.requireAll('spawner', 'position')
 
-local function generatePos(enemy)
-
-end
-
-local function randomOffsetFrom(position)
-  local previous_positions = {}
-  foreach(Enemies, generatePos)
-  vec(
-    spawner_position.x + flr(rnd(3))+1,
-    spawner_position.y + flr(rnd(3))+1
-  )
-end
-
 function spawnEnemies:process(entity, _dt)
   local spawner_position = entity.position
+  local enemy_positions = {x = {}, y = {}}
+  for x=spawner_position.x - 2, spawner_position.x + 2 do
+    add(enemy_positions.x, x)
+  end
+  for y=spawner_position.y - 2, spawner_position.y + 2 do
+    add(enemy_positions.y, y)
+  end
+
   while #Enemies < 3 do
-    local enemy = Enemy:new(randomOffsetFrom(spawner_position), 15, 15)
+    local xs = chance.helpers.shuffle(enemy_positions.x, 3)
+    local ys = chance.helpers.shuffle(enemy_positions.y, 3)
+    local position = vec(xs[1], ys[1])
+    local enemy = Enemy:new(position, 15, 15)
     world:add(enemy)
     add(Enemies, enemy)
   end
